639135ba461ca41d5223ba3442408221253ead08
jimmydrinkscoffee <jimmyvo.05122002@gmail.com>
2024-12-02 13:45:32 +0700

    make it O(n)

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -4,32 +4,33 @@
 #include <sstream>
 #include <vector>
 
-bool check_safe(const std::vector<int> &v) {
+int check_safe(const std::vector<int> &v) {
 	if (v.size() <= 1) {
-		return true;
+		return -1;
 	}
 
 	int inc = 0;
 	for (int i = 1; i < v.size(); i++) {
 		int d = v[i] - v[i - 1];
 		if (d * inc < 0 || std::abs(d) < 1 || std::abs(d) > 3) {
-			return false;
+			return i;
 		}
 		inc = d;
 	}
 
-	return true;
+	return -1;
 }
 
 bool check_safe_without_index(const std::vector<int> &v, int i) {
 	std::vector<int> tol = v;
 	tol.erase(tol.begin() + i);
-	return check_safe(tol);
+	return check_safe(tol) <= 0;
 }
 
 int main(int argc, char *argv[]) {
 	std::fstream f("input.txt");
 	std::string ln;
+	int d[4]  = {-2, -1, 0, 1};
 	int part1 = 0, part2 = 0;
 	while (std::getline(f, ln)) {
 		std::istringstream s(ln);
@@ -38,13 +39,17 @@ int main(int argc, char *argv[]) {
 		while (s >> n) {
 			v.push_back(n);
 		}
-		if (check_safe(v)) {
+		int i = check_safe(v);
+		if (i <= 0) {
 			part1++, part2++;
 		} else {
-			for (int j = 0; j < v.size(); j++) {
-				if (check_safe_without_index(v, j)) {
-					part2++;
-					break;
+			for (auto j : d) {
+				int k = i + j;
+				if (k >= 0 && k < v.size()) {
+					if (check_safe_without_index(v, k)) {
+						part2++;
+						break;
+					}
 				}
 			}
 		}
