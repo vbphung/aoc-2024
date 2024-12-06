dfcfa739815087f940471f6b9b1190906d7716c9
jimmydrinkscoffee <jimmyvo.05122002@gmail.com>
2024-12-06 15:54:51 +0700

    feat(06): my part 2 looks stupid af

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -15,10 +15,14 @@ fn main() {
     }
 
     let part_1 = patrol_area(area.clone());
-    let part_2 = 0;
+
+    let t = std::time::Instant::now();
+    let part_2 = stuck_poss(area);
+    let dur = t.elapsed();
 
     println!("{}", part_1);
     println!("{}", part_2);
+    println!("Part 2 took {:?}ms", dur.as_millis());
 }
 
 fn patrol_area(area: Vec<String>) -> i32 {
@@ -29,15 +33,10 @@ fn patrol_area(area: Vec<String>) -> i32 {
         return -1;
     }
 
-    let (mut dirx, mut diry) = match d {
-        '<' => (0, -1),
-        '>' => (0, 1),
-        '^' => (-1, 0),
-        _ => (1, 0),
-    };
-
+    let (mut dirx, mut diry) = dir(d);
     let mut area = area;
     let mut d = d.to_string();
+
     loop {
         let m = (x as i32) + dirx;
         let n = (y as i32) + diry;
@@ -61,11 +60,81 @@ fn patrol_area(area: Vec<String>) -> i32 {
         y = ny;
     }
 
-    for row in area {
-        println!("{}", row);
+    ans
+}
+
+fn stuck_poss(area: Vec<String>) -> i32 {
+    let mut ans = 0;
+
+    let (mut x, mut y, d) = find_guard(area.clone());
+    if x >= area.len() {
+        return -1;
+    }
+
+    let (mut dirx, mut diry) = dir(d);
+    let mut a = area;
+    let mut d = d.to_string();
+
+    loop {
+        let m = (x as i32) + dirx;
+        let n = (y as i32) + diry;
+        if m < 0 || m >= a.len() as i32 || n < 0 || n >= a[0].len() as i32 {
+            break;
+        }
+
+        let nx = m as usize;
+        let ny = n as usize;
+        let c = a[nx].chars().nth(ny).unwrap();
+        if c == '#' {
+            (dirx, diry, d) = turn_right(dirx, diry);
+            continue;
+        } else if c == '.' {
+            a[nx].replace_range(ny..ny + 1, "#");
+            if check_loop(a.clone(), x, y, d.chars().next().unwrap()) {
+                ans += 1;
+            }
+
+            a[nx].replace_range(ny..ny + 1, &d);
+        }
+
+        x = nx;
+        y = ny;
     }
 
     ans
+}
+
+fn check_loop(area: Vec<String>, x: usize, y: usize, d: char) -> bool {
+    let (mut x, mut y) = (x, y);
+    let (mut dirx, mut diry) = dir(d);
+    let mut area = area;
+    let mut d = d.to_string();
+
+    loop {
+        let m = (x as i32) + dirx;
+        let n = (y as i32) + diry;
+        if m < 0 || m >= area.len() as i32 || n < 0 || n >= area[0].len() as i32 {
+            break;
+        }
+
+        let nx = m as usize;
+        let ny = n as usize;
+
+        let c = area[nx].chars().nth(ny).unwrap();
+        if c == '#' {
+            (dirx, diry, d) = turn_right(dirx, diry);
+            continue;
+        } else if c == d.chars().next().unwrap() {
+            return true;
+        } else if c == '.' {
+            area[nx].replace_range(ny..ny + 1, &d);
+        }
+
+        x = nx;
+        y = ny;
+    }
+
+    false
 }
 
 fn find_guard(area: Vec<String>) -> (usize, usize, char) {
@@ -86,5 +155,14 @@ fn turn_right(dirx: i32, diry: i32) -> (i32, i32, String) {
         (-1, 0) => (0, 1, String::from(">")),
         (0, 1) => (1, 0, String::from("v")),
         _ => (0, -1, String::from("<")),
+    }
+}
+
+fn dir(d: char) -> (i32, i32) {
+    match d {
+        '<' => (0, -1),
+        '>' => (0, 1),
+        '^' => (-1, 0),
+        _ => (1, 0),
     }
 }
